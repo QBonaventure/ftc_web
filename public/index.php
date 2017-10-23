0ef7406ac43c3d457ec9cb3497ec8540bfc199b0
Quentin Bonaventure <quentin@bonaventure.xyz>
2017-10-23 17:22:14 -0400

    blah

--- public/index.php
+++ public/index.php
@@ -1,4 +1,23 @@
 <?php
 
-echo "Hi !";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+use Zend\Config\Config;
+
+// Change to the project root, to simplify resolving paths
+chdir(dirname(__DIR__));
+
+// Setup autoloading
+require 'vendor/autoload.php';
+
+$container = include 'config/container.php';
+$app       = $container->get(Zend\Expressive\Application::class);
+
+$app->pipeRoutingMiddleware();
+$app->pipeDispatchMiddleware();
+
+$preRunTime = microtime(true);
+
+$app->run();
